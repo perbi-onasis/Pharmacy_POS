e1568343f181233d48ebfa0b79281076066fe527
Perbi Onasis <onasisyawperbi@gmail.com>
2024-02-19 20:26:58 +0000

    Creating a github repo

---
+++ src/main/java/com/PharmacyPOs/Pharmacy_POS/PharmacyPosApplication.java
@@ -0,0 +1,13 @@
+package com.PharmacyPOs.Pharmacy_POS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyPosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmacyPosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PharmacyPOs/Pharmacy_POS/PharmacyPosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PharmacyPOs.Pharmacy_POS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyPosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
