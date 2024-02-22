34b41db5bcefeb0f8947999c9afaf78837de713f
Perbi Onasis <onasisyawperbi@gmail.com>
2024-02-22 19:35:12 +0000

    Added UserService to handle user registration and authentication logic

---
+++ src/main/java/com/PharmacyPOs/Pharmacy_POS/user_account/UserService.java
@@ -0,0 +1,23 @@
+package com.PharmacyPOs.Pharmacy_POS.user_account;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public User registerUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    public User findUserByEmail(String email){
+        return userRepository.findByEmail(email);
+    }
+}
