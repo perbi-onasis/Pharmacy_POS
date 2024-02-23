57a4c9ee5ae32f65b2dc7c656a44ff5a3af666a4
Perbi Onasis <onasisyawperbi@gmail.com>
2024-02-23 17:14:56 +0000

    Used an annotation to suppress unnesscessary deprecation warnings

--- src/main/java/com/PharmacyPOs/Pharmacy_POS/user_account/User.java
+++ src/main/java/com/PharmacyPOs/Pharmacy_POS/user_account/User.java
@@ -8,41 +8,55 @@ import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@SuppressWarnings("deprecation")
 @Document(collection = "users")
 public class User {
 
     @Id
     private String id;
 
+    @SuppressWarnings("deprecation")
     @NotBlank(message = "First name is required")
     private String firstname;
 
+    // @SuppressWarnings("deprecation")
     @NotBlank(message = "Surname is required")
     private String surname;
 
+    // @SuppressWarnings("deprecation")
     @NotBlank(message = "Phone number is required")
     @Pattern(regexp = "^\\+?[0-9]{10,15}$", message = "Phone number is not valid")
     private String phoneNumber;
 
+    // @SuppressWarnings("deprecation")
     @NotBlank(message = "Email is required")
     @Email(message = "Email is not valid")
     private String email;
 
-
+    // @SuppressWarnings("deprecation")
     @NotBlank(message = "Pharmacy name is required")
     private String pharmacyName;
-
+    
+    // @SuppressWarnings("deprecation")
     @NotBlank(message = "Password is required")
     @Length(min = 8, message = "Password must be at least 8 characters long")
     private String password;
 
-
+    // @SuppressWarnings("deprecation")
     @NotBlank(message = "Location is required")
     private String location;
 
 
     @NotNull(message = "Pharmacy type is required")
     private PharmacyType pharmacyType;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
     public String getPassword() {
         return password;
